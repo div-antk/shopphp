393e0662a72d94adcab2142eb8451c3c9ac4a4d5
div-antk <51283468+owaii@users.noreply.github.com>
2020-02-18 22:15:38 +0900

    add pro_disp_image

--- product/pro_disp.php
+++ product/pro_disp.php
@@ -19,7 +19,7 @@ $password = '';
 $dbh = new PDO($dsn, $user, $password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = 'SELECT name,price FROM mst_product WHERE code=?';
+$sql = 'SELECT name,price,image FROM mst_product WHERE code=?';
 $stmt = $dbh->prepare($sql);
 $data[] = $pro_code;
 $stmt->execute($data);
@@ -27,8 +27,18 @@ $stmt->execute($data);
 $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 $pro_name=$rec['name'];
 $pro_price=$rec['price'];
+$pro_image_name=$rec['image'];
 
 $dbh = null;
+
+if($pro_image_name=='')
+{
+  $disp_image = '';
+}
+else
+{
+  $disp_image ='<img src="./image/'.$pro_image_name.'">';
+}
 
 }
 catch (Exception $e)
@@ -49,6 +59,8 @@ catch (Exception $e)
 <br>
 価格<br>
 <?php print $pro_price; ?>
+<br>
+<?php print $disp_image; ?>
 <br>
 <br>
 <form>
