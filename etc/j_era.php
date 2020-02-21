fff90521d0e8f790f00cb5e4a55232e61643d4a5
div-antk <51283468+owaii@users.noreply.github.com>
2020-02-21 20:43:28 +0900

    study function

---
+++ etc/j_era.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>ろくまる農園</title>
+  </head>
+<body>
+
+<?php
+
+$year = $_POST['year'];
+
+$j_cal = era($year);
+print $j_cal;
+function era($year)
+{
+  if(1868 <= $year && $year <= 1911)
+  {
+    $era = "明治";
+  }
+  elseif(1912 <= $year && $year <= 1925)
+  {
+    $era = "大正";
+  }
+  elseif(1926 <= $year && $year <= 1988)
+  {
+    $era = "昭和";
+  }
+  elseif(1989 <= $year && $year <= 2019)
+  {
+    $era = "平成";
+  }
+  elseif(2020 == $year)
+  {
+    $era = "令和";
+  }
+  else
+  {
+    print "無効な数値です";
+  }
+
+  return($era);
+}
+
+?>
+
+</body>
+</html>
