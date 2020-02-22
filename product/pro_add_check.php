14ecdb4805e81311abb54cec2ad29c8d605467a0
div-antk <51283468+owaii@users.noreply.github.com>
2020-02-22 11:31:07 +0900

    add function some_files

--- product/pro_add_check.php
+++ product/pro_add_check.php
@@ -27,12 +27,13 @@ else
 
 <?php
 
-$pro_name = $_POST['name'];
-$pro_price = $_POST['price'];
-$pro_image = $_FILES['image'];
+require_once('../common/common.php');
 
-$pro_name = htmlspecialchars($pro_name, ENT_QUOTES, 'UTF-8');
-$pro_price = htmlspecialchars($pro_price, ENT_QUOTES, 'UTF-8');
+$post = sanitize($_POST);
+
+$pro_name = $post['name'];
+$pro_price = $post['price'];
+$pro_image = $_FILES['image'];
 
 if($pro_name=='')
 {
--- product/pro_edit_check.php
+++ product/pro_edit_check.php
@@ -27,15 +27,16 @@ else
 
 <?php
 
-$pro_code = $_POST['code'];
-$pro_name = $_POST['name'];
-$pro_price = $_POST['price'];
-$pro_image_name_old = $_POST['image_name_old'];
-$pro_image = $_FILES['image'];
 
-$pro_code = htmlspecialchars($pro_code, ENT_QUOTES, 'UTF-8');
-$pro_name = htmlspecialchars($pro_name, ENT_QUOTES, 'UTF-8');
-$pro_price = htmlspecialchars($pro_price, ENT_QUOTES, 'UTF-8');
+require_once('../common/common.php');
+
+$post = sanitize($_POST);
+
+$pro_code = $post['code'];
+$pro_name = $post['name'];
+$pro_price = $post['price'];
+$pro_image_name_old = $post['image_name_old'];
+$pro_image = $_FILES['image'];
 
 if($pro_name=='')
 {
