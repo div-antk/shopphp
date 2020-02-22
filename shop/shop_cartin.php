df2c180a1d9d160931ad561f7dc93fd08736224e
div-antk <51283468+owaii@users.noreply.github.com>
2020-02-22 13:24:45 +0900

    product_to_cart

---
+++ shop/shop_cartin.php
@@ -0,0 +1,58 @@
+<?php
+
+session_start();
+session_regenerate_id(true);
+if(isset($_SESSION['member_login'])==false)
+{
+  print 'ようこそ、ゲスト様。';
+  print '<a href="member_login.html">会員ログイン</a><br>';
+  print '<br>';
+}
+else
+{
+  print 'ようこそ';
+  print $_SESSION['member_name'];
+  print '様<br>';
+  print '<a href="member_logout.php">ログアウト</a><br>';
+  print '<br>';
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>ろくまる農園</title>
+  </head>
+<body>
+
+<?php
+
+try
+{
+
+$pro_code = $_GET['procode'];
+
+if (isset($_SESSION['cart'])==true)
+{
+  $cart = $_SESSION['cart']; // 現在のカートの内容を$cartにコピーする
+}
+
+$cart[] = $pro_code; // カートに商品を追加
+$_SESSION['cart']=$cart; // $_SESSIONにカートを保管する
+
+}
+catch (PDOException $e)
+{
+  print 'ただいま障害により大変ご迷惑をおかけしております。<br>' .$e->getMessage()."<br>";
+  exit();
+}
+
+?>
+
+カートに追加しました。<br>
+<br>
+<a href="shop_list.php">商品一覧に戻る</a>
+
+</body>
+</html>
