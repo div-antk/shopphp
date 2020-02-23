06b78a538714989dad8c42aed808be023c05678a
div-antk <51283468+owaii@users.noreply.github.com>
2020-02-23 22:17:08 +0900

    fix cart_view

--- shop/shop_cartcheck.php
+++ shop/shop_cartcheck.php
@@ -92,23 +92,35 @@ catch (PDOException $e)
 ショッピングカート<br>
 <br>
 <form method="post" action="qty_change.php">
+<table border="1">
+<tr>
+<td> 商品 </td>
+<td> 商品画像 </td>
+<td> 価格 </td>
+<td> 数量 </td>
+<td> 小計 </td>
+<td> 削除 </td>
+</tr>
 <?php for($i=0; $i<$max; $i++)
 {
 ?>
-  <?php print $pro_name[$i]; ?>
-  <?php print $pro_image[$i]; ?>
-  <?php $total_price = $pro_price[$i] * $quantity[$i]; ?>
-  <?php print "${total_price}円"; ?>
-  <input type="checkbox" name="delete<?php print $i; ?>">
-  <input type="text" name="quantity<?php print $i; ?>" value="<?php print $quantity[$i]; ?>" style="width:40px">
+<tr>
+  <td><?php print $pro_name[$i]; ?></td>
+  <td><?php print $pro_image[$i]; ?></td>
+  <td><?php $total_price = $pro_price[$i] * $quantity[$i]; ?></td>
+  <td><?php print "${total_price}円"; ?></td>
+  <td><input type="text" name="quantity<?php print $i; ?>" value="<?php print $quantity[$i]; ?>" style="width:40px"></td>
+  <td><input type="checkbox" name="delete<?php print $i; ?>"></td>
   <br>
+</tr>
 <?php
 }
 ?>
-
+</table>
 <input type="hidden" name="max" value="<?php print $max; ?>">
 <input type="submit" value="数量変更"><br>
-<input type="button" onclick="history.back()" value="戻る">
+<a href="shop_list.php">商品一覧に戻る</a>
+<!-- <input type="button" onclick="history.back()" value="戻る"> -->
 </form>
 
 </body>
