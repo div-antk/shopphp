91c7aaaaa037fa404ca6f653aa4dcfa2bb03de4b
div-antk <51283468+owaii@users.noreply.github.com>
2020-02-24 16:31:59 +0900

    add order_done

---
+++ shop/order_form_done.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>ろくまる農園</title>
+  </head>
+<body>
+
+<?php
+
+require_once('../common/common.php');
+
+$post = sanitize($_POST);
+
+$name = $post['name'];
+$email = $post['email'];
+$postal_code1 = $post['postal_code1'];
+$postal_code2 = $post['postal_code2'];
+$address = $post['address'];
+$tel = $post['tel'];
+
+print "${name}様、ご注文ありがとうございました！<br><br>";
+print "${email} にメールを送りましたので、ご確認ください。<br>";
+print "商品は以下の住所に発送させていただきます。<br><br>";
+print "${postal_code1}-${postal_code2}<br>";
+print "${address}<br><br>";
+print "${tel}<br>";
+
+?>
+
+</body>
+</html>
