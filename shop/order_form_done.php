b6b8a70501ade4f479d58d68b317aea10a47ac2e
div-antk <51283468+owaii@users.noreply.github.com>
2020-02-25 22:27:18 +0900

    add autoreply_mail

--- shop/order_form_done.php
+++ shop/order_form_done.php
@@ -35,6 +35,74 @@ print "${postal_code1}-${postal_code2}<br>";
 print "${address}<br><br>";
 print "${tel}<br>";
 
+$text = '';
+$text .= $name." 様 \n\n このたびは、当ショップをご利用いただきありがとうございました。\n";
+$text .= "\n";
+$text .= "ご注文商品\n";
+$text .= "--------------------\n";
+
+$cart = $_SESSION['cart'];
+$quantity = $_SESSION['quantity'];
+$max = count($cart);
+
+$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+for($i=0; $i<$max; $i++)
+{
+  $sql = 'SELECT name,price FROM mst_product WHERE code=?';
+  $stmt = $dbh->prepare($sql);
+  $data[0] = $cart[$i];
+  $stmt->execute($data);
+
+  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  $name = $rec['name'];
+  $price = $rec['price'];
+  $total_quantity = $quantity[$i];
+  $total_price = $price * $total_quantity;
+
+  $text .= $name.' ';
+  $text .= $price.'円 x ';
+  $text .= $total_quantity.'個 = ';
+  $text .= $total_price."円 \n";
+}
+
+$dbh = null;
+
+$text .= "送料は無料です\n";
+$text .= "--------------------\n";
+$text .= "\n";
+$text .= "代金は以下の口座にお振込みください。\n";
+$text .= "モノポリー銀行 ボードウォーク支店 普通 1234567\n";
+$text .= "入金が確認出来次第、発送いたします。\n";
+$text .= "\n";
+$text .= "////////////////////\n";
+$text .= "\n";
+$text .= "株式会社はたらくねこ\n";
+$text .= "\n";
+$text .= "////////////////////\n";
+
+// print '<br>';
+// print nl2br($text);
+
+$title = 'ご注文ありがとうございます！';
+$header = 'From: info@hatarakuneko.co.jp';
+$text = html_entity_decode($text, ENT_QUOTES, 'UTF-8');
+mb_language('Japanese');
+mb_internal_encoding('UTF-8');
+mb_send_mail($email, $title, $text, $header);
+
+$title = 'お客様からのご注文がありました。';
+$header = 'From: '.$email;
+$text = html_entity_decode($text, ENT_QUOTES, 'UTF-8');
+mb_language('Japanese');
+mb_internal_encoding('UTF-8');
+mb_send_mail('info@hatarakuneko.co.jp', $title, $text, $header);
+
 }
 
 catch (PDOException $e)
